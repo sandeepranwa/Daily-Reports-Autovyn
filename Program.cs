64953878507ab9e16e4df1714a511123ef52baf2
Sandeep Ranwa <98248651+sandeepranwa@users.noreply.github.com>
2024-11-21 18:23:34 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ConsoleToWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder().Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder() =>
+             Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webHost =>
+            {
+                webHost.UseStartup<Startup>();
+            });
+
+    }
+}
+ 
---
+++ Startup.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace ConsoleToWebApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddTransient<CustomMiddleware1>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            //app.Run(async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello from run first");
+            //});
+
+            app.Use(async (context,next) =>
+            {
+                await context.Response.WriteAsync("Hello from run Use-1 1\n");
+                await next();
+
+                await context.Response.WriteAsync("Hello from run Use-1 2\n");
+            });
+
+            app.UseMiddleware<CustomMiddleware1>();
+
+            app.Map("/testmode", CoutomCode);
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello from run Use-3 1\n");
+                await next();
+
+                await context.Response.WriteAsync("Hello from run Use-3 2\n");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Respond send succesfully!!\n");
+                
+            });
+
+            //app.Run(async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello from run Second\n");
+            //});
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting(); 
+
+            app.UseEndpoints(endpoints =>
+            {
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("hello from new Web API");
+                //});
+
+                // endpoints.MapGet("/test", async context =>
+                // {
+                //     await context.Response.WriteAsync("hello from new Web API test mode");
+                // });
+
+
+                endpoints.MapControllers(); 
+            } );
+    }
+
+        private void CoutomCode(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("testmode is Respond send succesfully!!\n");
+
+            });
+        }
+
+     
+    }
+}
